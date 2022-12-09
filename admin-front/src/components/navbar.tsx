fec6170c40047af17f78701c43b2018bb68340c6
Nicolas <nickogarces@gmail.com>
2022-12-09 16:55:05 -0300

    add zod + improvement in typeuser hook

--- admin-front/src/components/navbar.tsx
+++ admin-front/src/components/navbar.tsx
@@ -1,21 +1,41 @@
 import { Link } from "react-router-dom";
 import { navItems } from '../ProjectConstants';
+import { userContext } from "../hooks/user";
+import { useContext } from "react";
 
 export default function Navbar() {
-    return (
-        <header className="nav" >
-            <div className="nav__logo">
-                <p>LOGO</p>
-            </div>
-            <nav>
-                <ul className="nav__ul">
-                    {navItems.map(x=>(
-                        <li key={x.path} className="nav__ul__li">
-                            <Link className="nav__ul__li__link" to={x.path}>{x.name}</Link>
+    const user = useContext(userContext)
+    if (user?.role === 'user') {
+        return (
+            <header className="nav" >
+                <div className="nav__logo">
+                    <p>LOGO</p>
+                </div>
+                <nav>
+                    <ul className="nav__ul">
+                        {navItems.map(x => (
+                            <li key={x.path} className="nav__ul__li">
+                                <Link className="nav__ul__li__link" to={x.path}>{x.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </header >
+        )
+    } else {
+        return (
+            <header className="nav" >
+                <div className="nav__logo">
+                    <p>LOGO</p>
+                </div>
+                <nav>
+                    <ul className="nav__ul">
+                        <li className="nav__ul__li">
+                            <Link className="nav__ul__li__link" to='/admin'>admin rol</Link>
                         </li>
-                    ))}
-                </ul>
-            </nav>
-        </header >
-    )
+                    </ul>
+                </nav>
+            </header >
+        )
+    }
 }
--- admin-front/src/hooks/user.tsx
+++ admin-front/src/hooks/user.tsx
@@ -1,28 +1,69 @@
 import React, {
     createContext, useState, useEffect,
 } from 'react';
-import { onAuthStateChanged } from 'firebase/auth'
+import { onAuthStateChanged, User } from 'firebase/auth'
 import { auth } from '../firebase'
+import { z } from 'zod'
 
 type TT = { children: React.ReactNode }
+const Claims = z.promise(
+    z.object({
+        authTime: z.date(),
+        claims: z.object({
+            aud: z.string(),
+            auth_time: z.number(),
+            email: z.string(),
+            email_verified: z.boolean(),
+            exp: z.number(),
+            firebase: z.object({}),
+            iat: z.number(),
+            iss: z.string(),
+            name: z.string(),
+            role: z.string().optional(),
+            sub: z.string(),
+            user_id: z.string()
+        }),
+        expirationTime: z.date(),
+        issuedAtTime: z.date(),
+        signInProvider: z.string(),
+        signInFactor: z.string().optional(),
+        token: z.string()
+    })
+)
 
 
-export const userContext = createContext<any>(null)
+const Usr = z.object({
+    providerId: z.string(),
+    email: z.string(),
+    accessToken: z.string(),
+    displayName: z.union([z.string(),z.null()]),
+    role: z.string().optional(),
+    getIdTokenResult: z.function().args(z.boolean()).returns(Claims).optional()
+})
+
+type Usr=z.infer<typeof Usr>
+
+
+
+export const userContext = createContext<Usr|null>(null)
 
 export function ProvideUser(props: TT) {
-    const [user, setUser] = useState<any>(null)
+    const [user, setUser] = useState<Usr|null>(null)
     //add on auth state change
     useEffect(() => {
         return () => {
             onAuthStateChanged(auth, async (fbUser) => {
+                console.log("user on auth change")
+                console.log(fbUser)
                 if (fbUser) {
-                    console.log("user on auth change")
-                    // console.log(Object.keys(fbUser))
+                    const validUser = Usr.parse(fbUser)
                     // console.log(fbUser)
-                    const idTokenResult = await fbUser.getIdTokenResult(true);
                     console.log('id token')
-                    console.log(idTokenResult.claims.role)
-                    setUser(fbUser)
+                    const idTokenResult = await fbUser.getIdTokenResult(true);
+                    if (idTokenResult.claims.role) {
+                        validUser.role = idTokenResult.claims.role;
+                    }
+                    setUser(validUser)
                 } else {
                     setUser(null)
                 }
