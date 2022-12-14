a2662c90cd852f2fd91f8752047a9cc5835e7d14
Nicolas <nickogarces@gmail.com>
2022-12-14 16:43:44 -0300

    fix blocking admin router when user is loading

--- admin-front/src/components/pages/AllowOnlyAdmin.tsx
+++ admin-front/src/components/pages/AllowOnlyAdmin.tsx
@@ -1,12 +1,22 @@
 import React, { useContext } from 'react'
 import { userContext } from '../../hooks/user'
 import { Navigate } from "react-router-dom";
+import { Spinner } from '@chakra-ui/react'
 
 export function AllowOnlyAdmin({ children }: { children: React.ReactNode }) {
-    const user = useContext(userContext)
-    if (user && user.role && user.role === 'admin') {
-        return <>{children}</>
+    const { user, userIsLoading } = useContext(userContext)
+
+    if (userIsLoading) {
+        return <div><Spinner thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl' /></div>
     }
 
-    return <Navigate to="/login" replace={true} />
+    if (user && user.role && user.role === 'admin') {
+        return <>{children}</>
+    } else {
+        return <Navigate to="/login" replace={true} />
+    }
 }
