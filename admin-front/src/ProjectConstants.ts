6dc47884a8f5b34fc4861cc13d7983d0092540e3
Nicolas <nickogarces@gmail.com>
2022-12-14 16:47:55 -0300

    delete ProjectContants

--- admin-front/src/ProjectConstants.ts
+++
@@ -1,8 +0,0 @@
-export const navItemsUser = [
-    { name: 'Home', path: '/' },
-    { name: 'LogIn', path: '/login' }]
-
-export const navItems = [
-        { name: 'Admin', path: '/admin' },
-        { name: 'Home', path: '/' },
-        { name: 'LogIn', path: '/login' }]
