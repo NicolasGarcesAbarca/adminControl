70872b5ee291fdc69341a043c8faf74196efaec1
Nicolas <nickogarces@gmail.com>
2022-12-14 15:05:59 -0300

    add types to user from firebase

--- admin-front/src/hooks/user.tsx
+++ admin-front/src/hooks/user.tsx
@@ -1,7 +1,7 @@
 import React, {
     createContext, useState, useEffect,
 } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth'
+import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../firebase'
 import { z } from 'zod'
 
@@ -31,34 +31,41 @@ const Claims = z.promise(
     })
 )
 
-
+//TODO add more fields
 const Usr = z.object({
+    uid: z.string(),
     providerId: z.string(),
+    proactiveRefresh: z.object(
+        {
+            errorBackoff: z.number(),
+            isRunning: z.boolean(),
+            timerId: z.union([z.null(), z.number()]),
+        }).optional(),
     email: z.string(),
     accessToken: z.string(),
-    displayName: z.union([z.string(),z.null()]),
+    displayName: z.union([z.string(), z.null()]),
     role: z.string().optional(),
     getIdTokenResult: z.function().args(z.boolean()).returns(Claims).optional()
 })
 
-type Usr=z.infer<typeof Usr>
+type Usr = z.infer<typeof Usr>
+interface TUserContext{
+    user: Usr | null;
+    userIsLoading: boolean;
+}
 
 
-
-export const userContext = createContext<Usr|null>(null)
+export const userContext = createContext<TUserContext>({user:null,userIsLoading:true})
 
 export function ProvideUser(props: TT) {
-    const [user, setUser] = useState<Usr|null>(null)
+    const [user, setUser] = useState<Usr | null>(null)
+    const [userLoading, setUserLoading] = useState(true)
     //add on auth state change
     useEffect(() => {
         return () => {
             onAuthStateChanged(auth, async (fbUser) => {
-                console.log("user on auth change")
-                console.log(fbUser)
                 if (fbUser) {
                     const validUser = Usr.parse(fbUser)
-                    // console.log(fbUser)
-                    console.log('id token')
                     const idTokenResult = await fbUser.getIdTokenResult(true);
                     if (idTokenResult.claims.role) {
                         validUser.role = idTokenResult.claims.role;
@@ -67,13 +74,14 @@ export function ProvideUser(props: TT) {
                 } else {
                     setUser(null)
                 }
+                setUserLoading(false)
             })
         }
     }, [])
 
     return (
-        <userContext.Provider value={user}>
+        <userContext.Provider value={{ user: user, userIsLoading: userLoading }}>
             {props.children}
-        </userContext.Provider>
+        </userContext.Provider >
     )
 }
