6aac2fc5b8dda6bec0c67f4bfcba77b33eedeb64
Nicolas <nickogarces@gmail.com>
2022-12-27 23:51:15 -0300

    style error page

--- admin-front/src/ErrorPage.tsx
+++ admin-front/src/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom";
+import { Flex, Box ,Text} from "@chakra-ui/react";
 interface RouteError {
   status: number;
   statusText: string;
@@ -8,12 +9,20 @@ interface RouteError {
 export default function ErrorPage() {
   const error = useRouteError() as RouteError;
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      h={'100vh'}
+    >
+      <Box>
+
+        <Text fontSize='4xl'>Oops!</Text>
+        <Text>Sorry, an unexpected error has occurred.</Text>
+        <Text>
+          Error msg: <i>{error.statusText || error.message}</i>
+        </Text>
+      </Box>
+    </Flex>
   );
 }
